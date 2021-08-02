96c8372ec18ef34fba5d4d547d2e14eaeaa4b9ce
seaboyz <seaboyz@hotmail.com>
2021-08-02 15:16:19 -0500

    http req object

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 import express from 'express'
 
+// http constants
+// import http from 'http'
+// console.log(http.METHODS)
+// console.log(http.STATUS_CODES)
+
 const app = express()
+
+// Routes
+app.get('/', (req, res) => {
+  console.log(req.headers)
+  console.log(req.url)
+  console.log(req.ip)
+  console.log(req.hostname)
+  console.log(req.method)
+  console.log(req.protocol)
+  console.log(req.path)
+  console.log(req.subdomains)
+  console.log(req.params) //app.get('user/:id')
+  console.log(req.query)
+  res.status(404).end()
+})
 
 app.listen(3000, (err) => {
   if (err) {
