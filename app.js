4487e3c2acbf2e00e9911d095b2815c18c0dfe89
seaboyz <seaboyz@hotmail.com>
2021-08-02 14:46:46 -0500

    Add "/" route

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ import express from 'express'
 
 const app = express()
 
+app.get('/', (req, res) => {
+  console.log(req.url)
+  res.send('<h1>Hello</h1>')
+})
+
 app.listen(3000, (err) => {
   if (err) {
     console.log('there was a problem', err)
