97d05e9b4b53b6f4f2799cb9dc5cbea963677748
seaboyz <seaboyz@hotmail.com>
2021-08-02 14:51:31 -0500

    Add redirect

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello</h1>')
 })
 
+app.get('/old', (req, res) => {
+  res.redirect(301, '/new')
+})
+
+app.get('/new', (req, res) => {
+  res.send('<h2>New</h2>')
+})
+
 app.listen(3000, (err) => {
   if (err) {
     console.log('there was a problem', err)
