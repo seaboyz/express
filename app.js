3a1372d7c67f0474dbcc2c994c138b4e1704a07e
seaboyz <seaboyz@hotmail.com>
2021-08-02 16:51:10 -0500

    Add POST request

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 // apply express.json() middleware to parse json
 app.use(express.json())
 
-// GET (reading data from server)
+/* *****GET (reading data from server)***** */
 // input a url or a fetch or axios from frontend
 // /api/channel
 app.get('/api/channel', (req, res) => {
@@ -25,6 +25,22 @@ app.get('/api/channel/:id', (req, res) => {
 
   const channel = data.channels.find((channel) => channel.id == id)
   res.send(channel)
+})
+
+/* *****POST (sending data to the server)***** */
+// input from form or postman
+// /api/channel
+app.post('/api/channel/', (req, res) => {
+  // add new channel then return new list
+  // respond with a 201(created)
+  let { name } = req.body
+  let ids = data.channels.map((_) => _.id)
+  let id = Math.max(...ids) + 1
+  let last_update = Date.now()
+  let newChannel = { id, name, last_update }
+  data.channels.push(newChannel)
+  res.status(201).json(newChannel)
+  console.log('POST', data.channels)
 })
 
 app.listen(3000, (err) => {
