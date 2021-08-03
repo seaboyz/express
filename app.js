791ebf547e001cbd4c2b8a1f573b11e2e1c994c5
seaboyz <seaboyz@hotmail.com>
2021-08-02 21:37:18 -0500

    Add routes

--- app.js
+++ app.js
@@ -9,3 +9,26 @@ app.listen(3000, (err) => {
   }
   console.log('listening on port 3000')
 })
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  // hanle root
+})
+
+app.get('/things/cars', (req, res) => {})
+
+app.post('/things/cars', (req, res) => {})
+
+app.get('/things/cars/:carId', (req, res) => {})
+
+app.post('/things/cars/:carId', (req, res) => {})
+
+app.listen(3000, (err) => {
+  if (err) {
+    console.log('there was a problem', err)
+    return
+  }
+  console.log('listening on port 3000')
+})
